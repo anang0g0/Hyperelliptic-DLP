9461d6b99a92d67f6fec8b03ae93a6460ee7b89e
Osamu Sakai <fumumue@protonmail.com>
2021-12-23 11:30:42 +0900

    cantor

---
+++ cantor/struct.h
@@ -0,0 +1,33 @@
+#define DEG 512
+
+/* -*- mode: C; coding:utf-8 -*- */
+#define N 100
+#define E 4
+
+
+//monomial
+typedef struct{
+  unsigned short n; //単項式の次数
+  unsigned short a; //単項式の係数
+} oterm;
+
+//polynomial
+typedef struct{
+  oterm t[DEG]; //単項式の配列として多項式を表現する
+} OP;
+
+typedef union {
+  unsigned short x[DEG]; //配列の添字を次数に、配列の値を係数に持つ多項式の表現
+  unsigned long long int e[DEG/4];
+} vec;
+
+
+//extra gcd
+typedef struct{
+  OP u; //inverse of polynomial?
+  OP v; //error locater
+  OP d; //gcd
+} EX;
+
+
+
