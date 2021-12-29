dbcdbeff082fffa11b96d5a59ce067637c8868e2
Osamu Sakai <fumumue@protonmail.com>
2021-12-29 16:14:57 +0900

    addition

--- cantor/ecole-p.c
+++ cantor/ecole-p.c
@@ -1065,7 +1065,7 @@ if(LT(omod(omul(v,uu1),uu2)).a==0){
   return v;
 }
 printf("can't\n");
-exit(1);
+//exit(1);
 }
 
 
@@ -1250,7 +1250,7 @@ return D2;
 
 Div cdbl(Div D,OP f){
 OP k,s,l,u,v,u1,u2,v2,ut,vt,vc;
-
+Div D2;
 
 u=D.u;
 v=D.v;
@@ -1258,6 +1258,8 @@ if(chkdiv(D,f)==-1){
   printf("bakayo\n");
   exit(1);
 }
+
+OP t={0};
 
 k=odiv(osub(f,omul(v,v)),u);
 s=omod(omul(k,qinv(scr(2,v),u)),u);
@@ -1268,11 +1270,15 @@ v2=omod(minus(oadd(l,v)),u2);
 
 printpoln(o2v(u2));
 printpoln(o2v(omul(u,u)));
-if(oequ(omul(u,u),u2)==0 && oequ(omod(v2,u),omod(v,u))==0 && LT(omod(osub(omul(v2,v2),f),v2)).a==0){
-D.u=u2;
-D.v=v2;
+D2.u=u2;
+D2.v=v2;
+
+//if(LT(omod(omul(u,u),u2)).a==0 && 
+//if(oequ(omod(v2,u),omod(v,u))==0 || LT(omod(osub(omul(v2,v2),f),v2)).a==0){
+t=omod(osub(omul(v2, v2), (f)), u2);
+if(LT(t).a==0){
 printf("ii!\n");
-return D;
+return D2;
 }
 printf("dame\n");
 exit(1);
@@ -1366,6 +1372,63 @@ D3.u=ud;
 D3.v=vd;
 
 return D3;
+}
+
+Div dobule(Div D, OP ff){
+vec vx=o2v(ff);
+unsigned short d0,uv010,uu1,uu0,uuv11,d1,d2,d3,d4,d5,f2,e0,e1,f1,f3,mm0,mm1,mm3,mm4,s1,s2,l1,l0,l2,l3,u31,u30,v31,v30,uue0,uue1,iv,uuv01,uuv10,uuv00,u1,u0,v1,v0;
+OP u,v;
+Div D2;
+f3=vx.x[3];
+f2=vx.x[2];
+f1=vx.x[1];
+
+u=D.u;
+v=D.v;
+memset(vx.x,0,sizeof(vx.x));
+vx=o2v(u);
+u1=vx.x[1];
+u0=vx.x[0];
+
+memset(vx.x,0,sizeof(vx.x));
+vx=o2v(v);
+v1=vx.x[1];
+v0=vx.x[0];
+
+uu1=(u1*u1)%P; uu0=(u1*u0)%P; uuv01=(u0*v1)%P; uuv10=(u1*v0)%P; uuv11=(u1*v1)%P;
+uuv00=(u0*v0)%P;
+
+  d0=(6*v1*uu1-uuv10)%P; d1= (-4*uuv11+4*v0)%P; d2= (2*v1)%P;
+  d3=(6*v1*uu0-6*uuv00)%P; d4= (-4*uuv01)%P; d5=(2*v0)%P;
+  e0=(5*(-u1*uu1+2*uu0)-3*f3*u1+2*f2)%P;
+  e1=(5*(-u0+uu0+u0*u0)-3*f3*u0+f1)%P;
+  mm0=(d3-d5*(uu1-u0))%P; mm1=(d4-d5*(-u1))%P;
+  mm3=(d0-d2*(uu1-u0))%P; mm4=(d1-d2*(-u1))%P;
+  s1=(e1-d5*v1)%P; s2=(e0-d2*v1)%P;
+  iv=inv((mm0*mm4-mm1*mm3),P);
+  l3= (iv*(mm4*s1-mm1*s2))%P; l2=(iv*(mm0*s2-mm3*s1))%P;
+  l1=(v1+u1*l2-(uu1-u0)*l3)%P; l0=(v0+u0*l2-(uu0)*l3)%P;
+  u31=(2*l3*l2-2*u1-1)%P;
+  u30=(2*l3*l1+l2*l2-2*u0-uu1-2*u31*u1)%P;
+  uue1=(u31*u31)%P; uue0=(u31*u30)%P;
+  v31=((uu1-u30)*l3-u31*l2+l1)%P;
+  v30=(uue0*l3-u30*l2+l0)%P;
+
+memset(vx.x,0,sizeof(vx.x));
+vx.x[1]=u31;
+vx.x[0]=u30;
+vx.x[2]=1;
+u=v2o(vx);
+memset(vx.x,0,sizeof(vx.x));
+vx.x[1]=v31;
+vx.x[0]=v30;
+vx.x[2]=1;
+v=v2o(vx);
+
+D2.u=u;
+D2.v=v;
+
+return D2;
 }
 
 Div g2add(OP ff, OP uu1, OP uu2, OP vv1, OP vv2)
@@ -1555,7 +1618,13 @@ while(1){
 G1=gendiv(ff);
 X=gendiv(ff);
 G0=cadd(ff,X.u,G1.u,X.v,G1.v);
-//G0=cdbl(X,ff);
+//G0=dobule(X,ff);
+/*
+while((LT(xgcd(X.v,X.u).d).a!=1)){
+  X=gendiv(ff);
+  //G0=cdbl(X,ff);
+}
+*/
 if(chkdiv(G0,ff)==-1)
 {
   V=xgcd(X.u,G1.u);
@@ -1566,7 +1635,7 @@ if(chkdiv(G0,ff)==-1)
   count++;
   }
 }else{
-  printf("イイっ！この因子すげえいいっ！\n");
+  printf("ウホッ！いい因子。\n");
   xount++;
   exit(1);
 }
